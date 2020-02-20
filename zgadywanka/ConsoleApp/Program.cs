2758cbfc897386bbd715af7cf0bbf9a10fef9ff7
zsalach <61181802+zsalach@users.noreply.github.com>
2020-02-20 19:20:52 +0100

    wersja monolityczna

--- zgadywanka/ConsoleApp/Program.cs
+++ zgadywanka/ConsoleApp/Program.cs
@@ -13,36 +13,43 @@ namespace ConsoleApp
 
             Random los = new Random(); // deklaruję zmienną los i przypisuje do typu Random jednocześnie tworzę obiekt.
             int wylosowana = los.Next(1, 100+1);  // losuje z przedziału 
-            Console.WriteLine(wylosowana);
+           // Console.WriteLine(wylosowana);
             Console.WriteLine("Wylosowano liczbę od 1 do 100. \nOdgadnij ją");
+            // dopuki nie odgadniesz
 
-            //2. człowiek proponuje.
-            Console.WriteLine("Podaj swoją propozycję");
-            // int propozycja = int.Parse(Console.ReadLine()); // Parsujemy dane wprowadzone z klawiatury na INT32
-            int propozycja = Convert.ToInt32(Console.ReadLine());
+            Boolean a = true;
+            while(a)
+            {
+                //2. człowiek proponuje.
+                Console.WriteLine("Podaj swoją propozycję");
+                // int propozycja = int.Parse(Console.ReadLine()); // Parsujemy dane wprowadzone z klawiatury na INT32
+                int propozycja = Convert.ToInt32(Console.ReadLine());
+                
+                //3. komputer ocenia.
 
-            //3. komputer ocenia.
-
-            if (propozycja < wylosowana)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Za mało");
-                Console.ResetColor();
+                if (propozycja < wylosowana)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Za mało");
+                    Console.ResetColor();
+                }
+                else if (propozycja > wylosowana)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Za dużo ");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green; //aby wyłączyć konsole musimu ją zresetować.
+                                                                  //Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Trafiono!!!");
+                    Console.ResetColor(); // resetujemy kolor konsoli.
+                    a = false;
+                }
+                //Console.WriteLine("Koniec gry");
             }
-            else if (propozycja > wylosowana)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Za dużo ");
-                Console.ResetColor();
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Green; //aby wyłączyć konsole musimu ją zresetować.
-                //Console.BackgroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Trafiono!!!");
-                Console.ResetColor(); // resetujemy kolor konsoli.
-            }
-            //4. 2 i 3 zapętlamy.
+            Console.WriteLine("Koniec gry");
         }
     }
 }
